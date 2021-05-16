40141dd7ecc67e35c3538fd42c2d2d2ca6246d15
ElenaPrepodobnaja <73935522+ElenaPrepodobnaja@users.noreply.github.com>
2021-05-16 13:40:53 +0300

    10

---
+++ README.md
@@ -0,0 +1,3 @@
+# goit-js-hw-10-food-service
+
+https://elenaprepodobnaja.github.io/goit-js-hw-10-food-service/
---
+++ src/theme.js
@@ -0,0 +1,38 @@
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+const refs = {
+    input: document.querySelector('.theme-switch__toggle'),
+    body: document.querySelector('body'),
+};
+
+savedTheme();
+
+refs.input.addEventListener('change', onSwitchThemeHandler);
+
+function savedTheme() {
+
+ if(localStorage.getItem('theme') === Theme.DARK) {
+      refs.body.classList.toggle(Theme.DARK);
+      refs.input.checked = true;
+     } else {
+      refs.body.classList.toggle(Theme.LIGHT);
+      refs.input.checked = false;
+};
+};
+
+function onSwitchThemeHandler(e) {
+    e.preventDefault();
+
+    if(e.target.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+        refs.body.classList.remove(Theme.LIGHT);
+        refs.body.classList.add(localStorage.getItem('theme'));
+    } else {
+        localStorage.setItem('theme', Theme.LIGHT);
+        refs.body.classList.remove(Theme.DARK);
+    };
+};
